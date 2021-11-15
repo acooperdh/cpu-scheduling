b4bf050fa905a8063d412fb5151f096690ae5f50
acooper <an538926@dal.ca>
2021-11-15 12:53:36 -0400

    NPSJF complete, added output to Output.txt

--- asn4.c
+++ asn4.c
@@ -6,10 +6,26 @@
 
 #define TASK_SIZE 8
 
-// first come first serve method working properly, needs to output to file
-int fcfs(void* q){
+void sort_by_task_number(void* q){
+    if (q == NULL) return;
     Queue* queue = (Queue*)q;
-    printf("FCSC:\n");
+    for(int i = 0; i < queue->size; i++){
+        for(int j = 0; j < queue->size - 1; j++){
+            void* first_element = queue_get_element(queue, j);
+            void* second_element = queue_get_element(queue, j+1);
+            if(((Task*)first_element)->number > ((Task*)second_element)->number){
+                void* temp = queue_remove_element(queue, j);
+                queue_enqueue(queue, temp);
+            }
+        }
+    }
+    return;
+}
+    
+// first come first serve method working properly, needs to output to file
+int fcfs(void* q, FILE* fp){
+    Queue* queue = (Queue*)q;
+    fprintf(fp,"FCSC:\n");
     int current_time = 0;
     int num_of_tasks = queue->size;
     double average_wait_time;
@@ -22,21 +38,21 @@ int fcfs(void* q){
         task->end_time = current_time;
         task->wait_time = task->start_time - task->arrival;
         total_wait_time += task->wait_time;
-        printf("T%d\t%d\t%d\n", task->number, task->start_time, task->end_time);
+        fprintf(fp,"T%d\t%d\t%d\n", task->number, task->start_time, task->end_time);
     }
     average_wait_time = (double)total_wait_time / (double)queue->size;
     for(int i = 0; i < queue->size; i++){
         void* next_element = queue_get_element(queue, i);
-        printf("Waiting Time T%d: %d\n", ((Task*)next_element)->number, ((Task*)next_element)->wait_time);
+        fprintf(fp,"Waiting Time T%d: %d\n", ((Task*)next_element)->number, ((Task*)next_element)->wait_time);
     }
-    printf("Average Waiting Time: %.2f\n", average_wait_time); 
+    fprintf(fp,"Average Waiting Time: %.2f\n", average_wait_time); 
     return 0;
 }
 
 // time quantum is 4ms
 // if a new tasks arrives at the same time as a task that is old
 // the new one goes first in the queue 
-int round_robin(void* q){
+int round_robin(void* q, FILE* fp){
     int time_quantum = 4;
     int current_time = 0;;
     int total_wait_time = 0;
@@ -44,7 +60,7 @@ int round_robin(void* q){
     Queue* queue = (Queue*)q;
     Queue* finished_queue = queue_initialize(sizeof(int)*TASK_SIZE);
     int num_of_tasks = queue->size;
-    printf("RR:\n");
+    fprintf(fp, "\nRR:\n");
     while(queue->size > 0){
         void* next_element = queue_remove_element(queue, 0);
         Task* task = (Task*)next_element;
@@ -53,38 +69,74 @@ int round_robin(void* q){
             current_time += task->remaining;
             task->end_time = current_time;
             task->wait_time += task->start_time - task->arrival;
-            printf("T%d\t%d\t%d\n", task->number, task->start_time, task->end_time);
+            fprintf(fp,"T%d\t%d\t%d\n", task->number, task->start_time, task->end_time);
             queue_enqueue(finished_queue, task);
         }
         else{
             current_time += time_quantum;
-            printf("T%d\t%d\t%d\n", task->number, task->start_time, current_time);
+            fprintf(fp,"T%d\t%d\t%d\n", task->number, task->start_time, current_time);
             task->wait_time -= time_quantum;
             task->remaining -= time_quantum;
             queue_enqueue(queue, task);
         }
     }
-    // sorting finished queue by task number 
-    // REWORK IF POSSIBLE
-    for(int i = 0; i < finished_queue->size; i++){
-        for(int j = 0; j < finished_queue->size - 1; j++){
-            void* first_element = queue_get_element(finished_queue, j);
-            void* second_element = queue_get_element(finished_queue, j+1);
-            if(((Task*)first_element)->number > ((Task*)second_element)->number){
-                void* temp = queue_remove_element(finished_queue, j);
-                queue_enqueue(finished_queue, temp);
-            }
-        }
-    }
+    // sorting finished task queue by task number 
+    sort_by_task_number((void*)finished_queue);
 
     for(int i = 0; i < finished_queue->size; i++){
         void* next_element = queue_get_element(finished_queue, i);
         Task* task = (Task*)next_element;
         total_wait_time += task->wait_time;
-        printf("Waiting Time T%d: %d\n", task->number, task->wait_time);
+        fprintf(fp, "Waiting Time T%d: %d\n", task->number, task->wait_time);
     }
     average_wait_time = (double)total_wait_time / (double)finished_queue->size;
-    printf("Average Waiting Time: %.2f\n", average_wait_time);
+    fprintf(fp, "Average Waiting Time: %.2f\n", average_wait_time);
+    return 0;
+}
+// non preemptive shortest job first 
+int npsjf(void* q, FILE* fp){
+    fprintf(fp, "\nNPSJF:\n");
+    Queue* queue = (Queue*)q;
+    int current_time = 0;
+    int total_wait_time = 0;
+    double average_wait_time;
+    int num_of_tasks = queue->size;
+    void* first_task = queue_peek(queue);
+    Task* task = (Task*)first_task;
+    int first_burst = task->burst;
+    // after the first task is completed, the next tasks selected are according to burst time 
+    // and not arrival time
+    for(int i = 1; i < queue->size; i++){
+        for(int j = 2; j < queue->size - 1; j++){
+            void* first_element = queue_get_element(queue, j);
+            void* second_element = queue_get_element(queue, j+1);
+            if(((Task*)first_element)->burst > ((Task*)second_element)->burst && ((Task*)second_element)->arrival < first_burst){
+                void* temp = queue_remove_element(queue, j);
+                queue_enqueue(queue, temp);
+            }
+        }
+    }
+    for(int i = 0; i < queue->size; i++){
+        void* next_element = queue_get_element(queue, i);
+        Task* task = (Task*)next_element;
+        task->start_time = current_time;
+        current_time += task->burst;
+        task->end_time = current_time;
+        task->wait_time = task->start_time - task->arrival;
+        fprintf(fp,"T%d\t%d\t%d\n", task->number, task->start_time, task->end_time);
+    }
+
+    sort_by_task_number((void*)queue);
+    while(queue->size > 0){
+        void* next_element = queue_remove_element(queue, 0);
+        Task* task = (Task*)next_element;
+        total_wait_time += task->wait_time;
+        fprintf(fp, "Waiting Time T%d: %d\n", task->number, task->wait_time);
+    }
+    queue_destroy(queue);
+    average_wait_time = (double)total_wait_time / (double)num_of_tasks;
+    fprintf(fp, "Average Waiting Time: %.2f\n", average_wait_time);
+    
     return 0;
 }
 
@@ -103,6 +155,7 @@ int main(){
     }
     Queue *inital_tasks = queue_initialize(sizeof(int)*TASK_SIZE);
     Queue *rr_tasks = queue_initialize(sizeof(int)*TASK_SIZE);
+    Queue *npsjf_tasks = queue_initialize(sizeof(int)*TASK_SIZE);
     while(fgets(inputLine, sizeof(inputLine), fp) != NULL){
         printf("%s\n", inputLine);    
         taskNumber = atoi(strtok(inputLine, "T ,\n"));
@@ -127,9 +180,23 @@ int main(){
         new_task->wait_time = 0;
         queue_enqueue(rr_tasks, new_task);
     }
+    for(int i = 0; i < inital_tasks->size; i++){
+        void* next_element = queue_get_element(inital_tasks, i);
+        Task* new_task = malloc(sizeof(Task));
+        Task* old_task = (Task*)next_element;
+        new_task->number = old_task->number;
+        new_task->arrival = old_task->arrival;
+        new_task->burst = old_task->burst;
+        new_task->remaining = old_task->burst;
+        new_task->wait_time = 0;
+        queue_enqueue(npsjf_tasks, new_task);
+    }
+    FILE* output_file = fopen("Output.txt", "w");
     //FCFS implementationnt fcfs(inital_tasks);
-    fcfs((void*)inital_tasks);
-    round_robin((void*)rr_tasks);
+    fcfs((void*)inital_tasks, output_file);
+    round_robin((void*)rr_tasks, output_file);
+    npsjf((void*)npsjf_tasks, output_file);
+    fclose(output_file);
     fclose(fp);
 
     return 0;
